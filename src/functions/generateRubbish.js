dc95c488ba765b47a10c17822d4b94a07ca77dd8
Aroop 'FinlayDaG33k' Roelofs <me@finlaydag33k.nl>
2018-04-24 13:59:21 +0200

    Update the generateRubbish function to add spaces

--- src/functions/generateRubbish.js
+++ src/functions/generateRubbish.js
@@ -10,11 +10,19 @@ module.exports = {
   generateRubbish: function(data,high,low){
     // Get a random query length between the low value and high value
     var query_length = Math.floor(Math.random() * (high - low) + low);
-
+    
     // generate our rubbish query
     var rubbish = "";
-    for(i=0;i<query_length;i++){
-      rubbish += data[Math.floor(Math.random() * (data.length - 0) + 0)] + " ";
+    for(i=0;i<=query_length;i++){
+      // Get a random entry from the wordlist
+      rubbish += data[Math.floor(Math.random() * (data.length - 0) + 0)];
+      // check whether the current word is not the last
+      if(i != query_length){
+        // Check whether the query_length > 0, if so, add a space to the end
+        if(query_length > 0){
+          rubbish += " ";
+        }
+      }
     }
 
     return rubbish;
