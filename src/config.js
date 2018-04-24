c01cfeb9ee7f4c739efab99f5084cb77b5e28880
Aroop 'FinlayDaG33k' Roelofs <me@finlaydag33k.nl>
2018-04-24 21:48:52 +0200

    Add debug to config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  DEBUG: false, // set to false if you don't want debug messages
   QUERY_DELAY_SECONDS: 60, // Set the delay for queries in whole seconds.
   EXIT_ON_ERROR: "false"
 }
